231fc4b71678ebcf2b32ee961766a57bf5f585c3
David Rabassa <gestionarlaweb@gmail.com>
2020-10-27 15:57:02 +0100

    datos del pais usa

--- bin/main.dart
+++ bin/main.dart
@@ -1,12 +1,6 @@
-import 'package:apis/clases/reqres_respuesta.dart';
-import 'package:http/http.dart' as http; // alias http
 import 'package:apis/apis.dart' as apis;
 
 void main(List<String> arguments) {
-  final url = 'https://reqres.in/api/users?page=2';
-  http.get(url).then((res) {
-    final resReqRes = reqResRespuestaFromJson(res.body);
-    print('page: ${resReqRes.page}');
-    print('id del tercer elemento: ${resReqRes.data[2].id}');
-  });
+  // apis.getReqResp_service();
+  apis.getPais();
 }
--- lib/apis.dart
+++ lib/apis.dart
@@ -1,1 +1,24 @@
+import 'package:http/http.dart' as http; // alias http
+import 'clases/pais.dart';
 
+//import 'clases/reqres_respuesta.dart';
+
+void getPais() {
+  final pais = 'us';
+  final url = 'https://restcountries.eu/rest/v2/alpha/$pais';
+  http.get(url).then((res) {
+    final col = paisFromJson(res.body);
+
+    print('===================');
+    print('Pais: ${col.name}');
+    print('Población: ${col.population}');
+    print('Fronteras');
+    col.borders.forEach((f) => print(' $f'));
+    print('Idioma: ${col.languages[0].nativeName}');
+    print('Latitud: ${col.latlng[0]}');
+    print('Longitud: ${col.latlng[1]}');
+    print('Moneda: ${col.currencies[0].name}');
+    print('Bandera: ${col.flag}');
+    print('===================');
+  }).catchError((error) => print(error));
+}
