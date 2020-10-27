ba0b4911bcd3847f51bdc764609ce774ab99b0a3
David Rabassa <gestionarlaweb@gmail.com>
2020-10-27 12:44:01 +0100

    104-Mapear la respuesta a una instancia de clase

--- bin/main.dart
+++ bin/main.dart
@@ -1,14 +1,12 @@
-import 'dart:convert';
+import 'package:apis/clases/reqres_respuesta.dart';
 import 'package:http/http.dart' as http; // alias http
 import 'package:apis/apis.dart' as apis;
 
 void main(List<String> arguments) {
   final url = 'https://reqres.in/api/users?page=2';
   http.get(url).then((res) {
-    final body = jsonDecode(res.body);
-    print(body);
-    print('page: ${body['page']}');
-    print('per_page: ${body['per_page']}');
-    print('id del tercer elemento: ${body['data'][2]['id']}');
+    final resReqRes = reqResRespuestaFromJson(res.body);
+    print('page: ${resReqRes.page}');
+    print('id del tercer elemento: ${resReqRes.data[2].id}');
   });
 }
---
+++ lib/clases/reqres_respuesta.dart
@@ -0,0 +1,75 @@
+// Gestor de código
+// https://app.quicktype.io/
+
+import 'dart:convert';
+
+ReqResRespuesta reqResRespuestaFromJson(String str) =>
+    ReqResRespuesta.fromJson(json.decode(str));
+
+String reqResRespuestaToJson(ReqResRespuesta data) =>
+    json.encode(data.toJson());
+
+class ReqResRespuesta {
+  ReqResRespuesta({
+    this.page,
+    this.perPage,
+    this.total,
+    this.totalPages,
+    this.data,
+  });
+
+  int page;
+  int perPage;
+  int total;
+  int totalPages;
+  List<Persona> data;
+
+  factory ReqResRespuesta.fromJson(Map<String, dynamic> json) =>
+      ReqResRespuesta(
+        page: json['page'],
+        perPage: json["per_page"],
+        total: json["total"],
+        totalPages: json["total_pages"],
+        data: List<Persona>.from(json["data"].map((x) => Persona.fromJson(x))),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "page": page,
+        "per_page": perPage,
+        "total": total,
+        "total_pages": totalPages,
+        "data": List<dynamic>.from(data.map((x) => x.toJson())),
+      };
+}
+
+class Persona {
+  Persona({
+    this.id,
+    this.email,
+    this.firstName,
+    this.lastName,
+    this.avatar,
+  });
+
+  int id;
+  String email;
+  String firstName;
+  String lastName;
+  String avatar;
+
+  factory Persona.fromJson(Map<String, dynamic> json) => Persona(
+        id: json["id"],
+        email: json["email"],
+        firstName: json["first_name"],
+        lastName: json["last_name"],
+        avatar: json["avatar"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "email": email,
+        "first_name": firstName,
+        "last_name": lastName,
+        "avatar": avatar,
+      };
+}
